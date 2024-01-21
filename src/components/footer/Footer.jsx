3b911de3e23db1b613d9d139bb6c07fcd939fa76
muslim2210 <bukhori.muslim2210@gmail.com>
2024-01-21 13:04:05 +0700

    updated readme.md and updated logo

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ import "./footer.scss";
 import { Link } from "react-router-dom";
 
 import bg from "../../assets/footer-bg.jpg";
-import logo from "../../assets/tmovie.png";
+import logo from "../../assets/u-flix.png";
 
 const Footer = () => {
   return (
@@ -14,7 +14,7 @@ const Footer = () => {
         <div className="footer__content__logo">
           <div className="logo">
             <img src={logo} alt="" />
-            <Link to="/">tMovies</Link>
+            <Link to="/">U-Flix</Link>
           </div>
         </div>
         <div className="footer__content__menus">
@@ -38,7 +38,7 @@ const Footer = () => {
         </div>
         <div className="footer__content__copyright">
           <hr />
-          <p>© 2024 tMovies, Bukhori Muslim.</p>
+          <p>© 2024 U-flix, Bukhori Muslim. All rights reserved</p>
         </div>
       </div>
     </div>
